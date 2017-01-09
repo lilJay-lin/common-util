ab14e12eef5cc298b0a7cc00727cdd42519bc186
lilJay-lin <lin_xjie@foxmail.com>
2017-01-09 10:59:35 +0800

    删除案例入口页面

---
+++ src/pages/index/index.js
@@ -0,0 +1,9 @@
+/**
+ * Created by linxiaojie on 2017/1/3.
+ */
+const util = require('common/util')
+
+/*
+* string
+* */
+util.leftPad('1', 0)
--- src/pages/information2/index.js
+++
@@ -1,4 +0,0 @@
-/**
- * Created by linxiaojie on 2016/12/28.
- */
-console.log('i am index')
