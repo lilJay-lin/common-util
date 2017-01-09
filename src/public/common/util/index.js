a44f5d416330d2b59c4d64d88c33a0e465b087e8
lilJay-lin <lin_xjie@foxmail.com>
2017-01-09 10:57:38 +0800

    通过index.js导出所有util方法

---
+++ src/public/common/util/index.js
@@ -0,0 +1,12 @@
+/**
+ * Created by linxiaojie on 2016/6/7.
+ */
+/*
+ * 暴露其他库方法
+ * */
+export * from './object'
+export * from './collection'
+export * from './string'
+export * from './date'
+export * from './tpl'
+export * from './url'
