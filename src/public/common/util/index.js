c7d0cf4a97b7bcafe585122687a0e9a78c5426ae
lilJay-lin <lin_xjie@foxmail.com>
2017-03-09 17:09:44 +0800

    #fixed export * from **的形式，babel编译的代码在android4.0及以下有兼容问题

--- src/public/common/util/index.js
+++ src/public/common/util/index.js
@@ -4,11 +4,37 @@
 /*
  * 暴露其他库方法
  * */
-export * from './object'
-export * from './collection'
-export * from './string'
-export * from './date'
-export * from './tpl'
-export * from './url'
-export * from './dom'
-export * from './animate'
+/*
+*  babel-preset-es2015 loose配置编译出来的代码不一致，会出来：Cannot assign to read only property ''__esmodule' of object '#object''
+*  export * from ** 的形式会出现不一直的__esmodle代码：
+*  Object.defineProperty
+*  和 t.__esModule = true 共存报错
+*  rel: http://www.tuicool.com/articles/zuIjqiB
+*
+* export * from './object'
+ export * from './collection'
+ export * from './string'
+ export * from './date'
+ export * from './tpl'
+ export * from './url'
+ export * from './dom'
+ export * from './animate'
+* */
+import * as object from './object'
+import * as collection from './collection'
+import * as string from './string'
+import * as date from './date'
+import * as tpl from './tpl'
+import * as url from './url'
+import * as dom from './dom'
+import * as animate from './animate'
+
+var arrs = [object, collection, string, date, tpl, url, dom, animate]
+var util = {}
+collection.each(arrs, (obj) => {
+  collection.each(obj, (fn, key) => {
+    util[key] = fn
+  })
+})
+
+export default util
